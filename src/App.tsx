6d2facf59223b8cbffee92f28fdff1af4a694ec3
AnthoninHelias <heliasanthonin@yahoo.fr>
2024-01-29 10:56:24 +0100

    test du robot de deploiement gh

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with us 2
+          Learn React with us 23
         </a>
       </header>
     </div>
