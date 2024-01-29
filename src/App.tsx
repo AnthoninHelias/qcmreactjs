56eb51c198cc7d4e194e03a7b5b99239ba918e8d
Anthonin Helias <98337603+AnthoninHelias@users.noreply.github.com>
2024-01-29 11:47:46 +0100

    modfication workflow gh-file (#31)
    * modfication workflow
    
    * test du robot de deploiement gh
    
    * modification dans worflow de gh-files
    
    * modification dans worflow de gh-files
    
    * modification dans worflow de gh-files
    
    * modification dans worflow de gh-files
    
    * modification dans worflow de gh-files
    
    * modification dans worflow de gh-files
    
    * modification dans worflow de gh-files
    
    * modification dans worflow de gh-files
    
    * modification dans worflow de gh-files
    
    * suppression du check sur node bersion 18.X des workflows
    
    ---------
    
    Co-authored-by: AnthoninHelias <heliasanthonin@yahoo.fr>

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with us
+          Learn React with us 23
         </a>
       </header>
     </div>
