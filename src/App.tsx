890c64cf415cb32798f5fb006a0a862cc86651b9
Anthonin Helias <heliasantho@yahoo.fr>
2024-01-29 10:49:25 +0100

    modfication workflow

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with us
+          Learn React with us 2
         </a>
       </header>
     </div>
