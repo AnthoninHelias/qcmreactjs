fdb8fe86a1cb8e6461abfd980cb54bd98096eb20
schlanger <yehoudaschlanger@gmail.com>
2024-02-26 15:56:37 +0100

    Affichage des questions de manière dynamique

--- src/pages/QuestionsReponses.tsx
+++ src/pages/QuestionsReponses.tsx
@@ -1,52 +1,37 @@
 import './App.css';
 import * as React from 'react';
-import {useParams} from "react-router-dom";
-import {QuestionAnswer, questionAnswers} from "../data/questionsRéponses";
+import { useParams } from 'react-router-dom';
+import { questionAnswers } from "../data/questionsRéponses";
 
 function QuestionReponses() {
+    const { displayedText } = useParams<{ displayedText: string }>();
+    const [currentQuestionIndex, setCurrentQuestionIndex] = React.useState(0);
 
-    const {displayedText} = useParams<{displayedText: string}>();
+    const goToNextQuestion = () => {
+        if (currentQuestionIndex < questionAnswers.length - 1) {
+            setCurrentQuestionIndex(currentQuestionIndex + 1);
+        }
+    };
 
-    questionAnswers.map((questionAnswer: QuestionAnswer) => {
-        // ce console.log est inutile mais une erreur survient si on ne la met pas
-        console.log(questionAnswer.question);
-        questionAnswer.answer.map((answer:{title:string, id:number, correct:boolean} ) => {
-            // ce console.log est inutile mais une erreur survient si on ne la met pas
-            console.log(answer.title);
-        });
-    });
+    const currentQuestion = questionAnswers[currentQuestionIndex];
 
-    let index:number = 0;
-    const answer = questionAnswers[index];
-    const question = questionAnswers[index].question;
-    // cette déclaration est inutile mais une erreur survient si on ne la met pas
-    index=0;
+
     return (
         <div className="App">
             <header className="App-header">
-
-                <a>
-                    Hello Questions
-
-                </a>
-                <p>Nom du joueur : {displayedText}</p>
-                {
-                    // affiche la question
-                    question
-                }
-                {
-                    // affiche la réponse
-                    answer.answer.map((answer:{title:string, id:number, correct:boolean} ) => { return <p>{answer.title} </p> })
-                }
-
-
-
-
+                <p>Bonjour: {displayedText}</p>
+                <h2>{currentQuestion.question}</h2>
+                <ul>
+                    {currentQuestion.answer.map((answer) => (
+                        <li key={answer.id}>
+                            {answer.title}
+                        </li>
+                    ))}
+                </ul>
+                <button onClick={goToNextQuestion}>Question suivante</button>
             </header>
-
         </div>
     );
 }
-
 
 export default QuestionReponses;
