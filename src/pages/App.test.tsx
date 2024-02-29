b8ae77cc35049a4e4f744641661f4e3b0e552c4a
Anthonin Helias <heliasantho@yahoo.fr>
2024-02-29 15:40:45 +0100

    ajout test unitaire vérification  boutton cliqué

--- src/pages/App.test.tsx
+++ src/pages/App.test.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import {render, fireEvent, getByPlaceholderText , screen} from '@testing-library/react';
+import {fireEvent, render, screen} from '@testing-library/react';
 import App from './App';
 import {MemoryRouter} from "react-router-dom";
 import InputClick from "../components/InputClick";
+import ButtonClick from "../components/ButtonClick";
 
 
 describe('App', () => {
@@ -13,32 +14,10 @@ describe('App', () => {
         </MemoryRouter>));
 
   });
-/*
-    it('handles input change correctly', () => {
-        render(
-            (<MemoryRouter>
-                <App />)
-            </MemoryRouter>));
-        const inputElement = screen.getByPlaceholderText('Entrez un nom');
-
-        fireEvent.change(inputElement, { target: { value: 'John' } });
-
-        expect(inputElement.value).toBe('John');
-    });
-/*
-  it('updates displayedText on button click', () => {
-    const { getByPlaceholderText, getByText } = render(<App />);
-    const inputElement = getByPlaceholderText('Entrez un nom');
-    const buttonElement = getByText('Valider');
-
-    fireEvent.change(inputElement, { target: { value: 'John' } });
-    fireEvent.click(buttonElement);
-
-    expect(getByText('John'));
-  });*/
 
 });
 describe('InputClick Component', () => {
+
     // Test case for rendering the InputClick component
     it('renders InputClick component correctly', () => {
         // Render the InputClick component
@@ -47,5 +26,17 @@ describe('InputClick Component', () => {
         // Add more assertions based on your component's content and structure
         expect(screen.getByPlaceholderText('Test Placeholder')).toBeInTheDocument();
     });
+    it('getClicked', () => {
+        const mockButtonClick = jest.fn();
+        render(
+        <ButtonClick onClick={mockButtonClick} disabled={false}>
+        Valider
+       </ButtonClick>)
+    const buttonElement = screen.getByText('Valider');
+    fireEvent.click(buttonElement);
+    expect(mockButtonClick).toHaveBeenCalled();
+    expect(buttonElement).not.toHaveAttribute('disabled');});
 });
+
+
 
