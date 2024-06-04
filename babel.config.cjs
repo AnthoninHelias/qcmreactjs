3f2985522c81f191b0bcc43d2defe368f04b1a8b
Anthonin Helias <heliasantho@yahoo.fr>
2024-06-04 15:53:47 +0200

    test workflow

--- babel.config.cjs
+++ babel.config.cjs
@@ -3,4 +3,16 @@ module.exports = {
               '@babel/preset-env',
               '@babel/preset-react',
               '@babel/preset-typescript'],
+    plugins: ['@babel/plugin-transform-modules-commonjs'],
+    testEnvironment: 'jsdom',
+    transform: {
+        '^.+\\.tsx?$': 'ts-jest',
+        '^.+\\.jsx?$': 'babel-jest',
+    },
+    transformIgnorePatterns: [
+        "/node_modules/(?!axios)",
+    ],
+    moduleNameMapper: {
+        '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
+    },
 };
